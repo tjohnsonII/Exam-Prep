bb47a5cb9c68a5110f41cf60044624fb37c15f4c
Timothy A Johnson II <tjohnson082083@gmail.com>
2019-02-15 15:24:20 -0500

    JavaScript.js

---
+++ JavaScript.js.js
@@ -0,0 +1,12 @@
+
+function myConnectionDefinition() {
+ document.getElementById("Connection").innerHTML = "Connection: In networking, a connection refers to pieces of related information that are transfered through a network. This generally infers that a connection is built before the data transfer (by following the procedures laid out in a protocol) and then is deconstructed at the at the end of the data transfer.";
+}
+
+function myPacketDefinition() {
+ document.getElementById("Packet").innerHTML = "Packet: A packet is, generally speaking, the most basic unit that is transfered over a network. When communicating over a network, packets are the envelopes that carry your data (in pieces) from one end point to the other.";
+}
+
+function myNetworkDefinition() {
+ document.getElementById("Network").innerHTML = "Network Interface: A network interface can refer to any kind of software interface to networking hardware. For instance, if you have two network cards in your computer, you can control and configure each network interface associated with them individually.";
+}
